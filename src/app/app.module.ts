5d076a888ce0b1b79d737dad8e353a5fe2edf755
Carl Edwin <carlinstr@gmail.com>
2019-12-25 18:29:57 -0300

    Implementação da tela de cadastro de PJ com validação de CPF, CNPJ e MASK

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,10 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material';
 
 //import { LoginModule } from './autenticacao/login/login.module';
-import { LoginModule, LoginRoutingModule } from './autenticacao';
+import { LoginModule,
+        LoginRoutingModule,
+        CadastroPjModule,
+      CadastroPjRoutingModule } from './autenticacao';
 import { AppRoutingModule } from './app-routing.module'; // o modulo root deve sempre ser o ultimo
 
 @NgModule({
@@ -20,6 +23,8 @@ import { AppRoutingModule } from './app-routing.module'; // o modulo root deve s
     MatButtonModule,
     LoginModule,
     LoginRoutingModule,
+    CadastroPjModule,
+    CadastroPjRoutingModule,
     AppRoutingModule
   ],
   // o modulo root deve sempre ser o ultimo, se importar um modulo filho apos um modulo pai, ele não sera reconhecido pela aplicacao
